c15f32012f2e1bd72574c8542fa315e96f03db59
thom234u <thomas@fage-butler.dk>
2022-11-09 17:28:09 +0100

    menu

---
+++ eraser racer/Assets/scripts/controlsbutton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controlsbutton : MonoBehaviour
+{
+    public void play9090()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ eraser racer/Assets/scripts/settingsbutton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class settingsbutton : MonoBehaviour
+{
+    public void settings()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
