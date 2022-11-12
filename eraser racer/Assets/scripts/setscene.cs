ca9b23cc86d110c1515b8e2d5107a6d49da8b31c
thom234u <thomas@fage-butler.dk>
2022-11-12 19:57:05 +0100

    allisgood

--- eraser racer/Assets/scripts/setscene.cs
+++ eraser racer/Assets/scripts/setscene.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class setscene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene(2);
     }
 
     // Update is called once per frame
