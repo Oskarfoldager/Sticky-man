66ec1865a6a0f6f1edb9408eec24b76709666d96
oskar ,ok <foldagerfamilie@gmail.com>
2022-11-16 19:00:10 +0100

    speedonerasers

--- eraser racer/Assets/scripts/force.cs
+++ eraser racer/Assets/scripts/force.cs
@@ -6,10 +6,12 @@ public class force : MonoBehaviour
     // Movement speed
     public float speed = 2;
     public Transform playertransform;
+    private float timeofnewspeed;
 
     // Use this for initialization
     void Start()
     {
+        timeofnewspeed = Time.time + 10;
         // Fly towards the right
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
     }
@@ -17,6 +19,12 @@ public class force : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Time.time > timeofnewspeed)
+        {
+            Debug.Log("newspeed");
+            speed *= 1.4f;
+            timeofnewspeed += 10;
+        }
         transform.position = new Vector3(transform.position.x,playertransform.position.y,transform.position.z);
     }
 }
